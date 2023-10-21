0f6165e96ca083510e9276e6fe519fa7749a8c93
Soli <asoliman96@gmail.com>
2023-10-21 13:03:55 +0200

    V0.0: Returns a list of user's ratings plus the prompt and saves them for later

--- plugin/main.py
+++ plugin/main.py
@@ -1,90 +1,51 @@
+
 import os
-from typing import Optional, List
-from fastapi import FastAPI, HTTPException, Depends
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
-from pydantic import BaseModel, Field
 import feedparser
 import pandas as pd
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
+from fastapi import FastAPI, Depends, Query
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
-# Initialize FastAPI
+
 app = FastAPI()
-# Add CORS middleware to allow requests from specified origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080", "https://chat.openai.com"],
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
+    allow_credentials=True,
 )
+app.mount("/.well-known", StaticFiles(directory=os.path.join(os.getcwd(), ".well-known")), name=".well-known")
 
-# Initialize SQLAlchemy
-engine = create_engine("sqlite:///books.db")
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-
-class Book(Base):
-    """Book model"""
-    __tablename__ = "books"
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    author = Column(String)
-    rating = Column(Integer)
-
-Base.metadata.create_all(bind=engine)
-
-# Define Pydantic models
-class BookBase(BaseModel):
-    from datetime import datetime
-    title: str
-    author: str
-    rating: int
-    read_at: Optional[datetime] = None
-
-class BookCreate(BookBase):
-    pass
-
-class Book(BookBase):
-    id: int
-
-    class Config:
-        orm_mode = True
-
-class GoodreadsProfile(BaseModel):
-    """Goodreads profile model"""
-    url: str
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-def get_goodreads_books(user_id: str):
-    """Fetches books from a Goodreads user's RSS feed and returns them as a DataFrame."""
+def get_goodreads_books(user_id: str, csv_file: str):
     url = f"https://www.goodreads.com/review/list_rss/{user_id}?shelf=%23ALL%23"
     feed = feedparser.parse(url)
     books = [{'title': entry['title'], 'author': entry['author_name'], 'rating': entry['user_rating']} for entry in feed.entries]
-    return pd.DataFrame(books)
+    df = pd.DataFrame(books)
+    df.to_csv(csv_file, index=False)
+    return df
 
-@app.post("/goodreads_profile")
-async def save_goodreads_profile(profile: GoodreadsProfile, db: Session = Depends(get_db)):
-    """Endpoint to save Goodreads profile data"""
-    user_id = profile.url.split('/')[-1]  # Extract user ID from URL
-    books_df = get_goodreads_books(user_id)
-    for index, row in books_df.iterrows():
-        book = Book(title=row['title'], author=row['author'], rating=row['rating'])
-        db.add(book)
-    db.commit()
-    return {"message": "Profile data saved successfully."}
+def get_books_df(profile_id: str) -> pd.DataFrame:
+    csv_file = f"data/{profile_id}.csv"
+    if os.path.isfile(csv_file):
+        return pd.read_csv(csv_file)
+    return get_goodreads_books(profile_id, csv_file)
 
-@app.get("/prompt", response_model=List[Book])
-def get_prompt(db: Session = Depends(get_db)):
-    """Endpoint to get book recommendations"""
-    books = db.query(Book).all()
-    return books
+
+@app.get("/prompt")
+def get_prompt(goodreads_profile_id: str = Query(..., min_length=6, regex="^[0-9]+$")):
+    """
+    Receives as input the [user's goodreads profile ID](https://help.goodreads.com/s/article/Where-can-I-find-my-user-ID) and returns JSON with a prompt and the user's book history.
+    """
+    if not goodreads_profile_id:
+        return {"message": "Please provide your Goodreads profile ID. This is the URL to learn more about it: https://help.goodreads.com/s/article/Where-can-I-find-my-user-ID"}
+    else:
+        books_df = get_books_df(goodreads_profile_id)
+        return {
+            "prompt": "Hi ChatGPT, I have compiled a list of the user's reading history below alognside their ratings. \
+                Please recommend a book for them that aligns with their interests. Clearly explain why you are recommending the books. \
+                    At the end ask them for extra information such as what genre they are interested in and try to make even better recommendations. \
+                        Do not make another request to me unless the user changes the Goodreads profile ID since I always return the same message for the same profile ID.",
+            "recent_books": books_df.head(10).to_dict('records'),
+            "other_books": books_df.tail(-10).to_dict('records')
+        }
